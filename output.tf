affb947bc2d975d63f7421b274fdf2bbd27eac61
saif25 <saif25@gmail.com>
2025-11-30 21:53:58 +0530

    chor: remove jenkins from main.tf, sg, output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,4 @@
-# output "ssh_connection_string_for_ec2_jenkins" {
-#   value = "ssh -i ~/Documents/keys/devops_proj1 ubuntu@${module.jenkins.jenkins_ec2_instance_public_ip}"
-# }
+
 
 # output "ssh_connection_string_for_ec2_sonar" {
 #   value = "ssh -i ~/Documents/keys/devops_proj1 ubuntu@${module.sonar.sonar_ec2_instance_public_ip}"

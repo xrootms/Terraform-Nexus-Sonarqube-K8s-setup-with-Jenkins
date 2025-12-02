ea1211db5053c1c5f912acc38c7291424c605179
saif25 <saif25@gmail.com>
2025-12-02 20:05:53 +0530

    chore: format terraform files

--- output.tf
+++ output.tf
@@ -8,9 +8,20 @@
 #   value = "ssh -i ~/Documents/keys/devops_proj1 ubuntu@${module.nexus.nexus_ec2_instance_public_ip}"
 # }
 
-# output "ssh_connection_string_for_k8s_master" {
-#   value = "ssh -i ~/Documents/keys/devops_proj1 ubuntu@${module.kubeadm-cluster.master_public_ip}"
+output "ssh_connection_string_for_k8s_master" {
+  value = "ssh -i ~/Documents/keys/devops_proj1 ubuntu@${module.kubeadm-cluster.master_public_ip}"
+}
+
+
+output "ssh_connection_string_for_k8s_worker_01" {
+  value = "ssh -i ~/Documents/keys/devops_proj1 ubuntu@${module.kubeadm-cluster.worker_public_ips[0]}"
+}
+
+# output "ssh_connection_string_for_k8s_worker_02" {
+#   value = "ssh -i ~/Documents/keys/devops_proj1 ubuntu@${module.kubeadm-cluster.worker_public_ips[1]}"
 # }
+
+
 
 
 # output "ssh_connection_string_for_k8s_worker_01" {
